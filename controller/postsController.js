68e4f97097511e4a210c622094e531e5890c6c97
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-01-22 15:50:09 +0100

    feat: show query

--- controller/postsController.js
+++ controller/postsController.js
@@ -5,15 +5,43 @@ const index = (req, res) => {
     const sql = `SELECT * FROM posts`
 
    connection.query(sql, (err, posts) =>{
-    if(err) return res.status(500).json({error : "Database query failed"});
-    res.json(posts);
-   })
+    if (err) {
+        return res.status(500).json({
+          message: "Errore interno del server",
+        });
+      } else {
+        return res.status(200).json({
+          status: "success",
+          data: posts,
+        });
+      }
+    });
 };
+
+
 
 //show
 const show = (req, res) => {
-
+    const id = req.params.id
+    const sql = `SELECT * FROM posts WHERE posts.id = ?`
     
+    connection.query(sql, [id], (err, posts) => {
+        if(err){
+            res.status(500).json({
+            message:"Errore interno al server"
+        });
+        }else if(posts.length=== 0){
+            res.status(404).json({
+                message:"post non trovato"
+            });
+        }else{
+            res.status(200).json({
+                status: "Success",
+                data: posts[0]
+            });
+        }
+
+    })
 
 };
 
